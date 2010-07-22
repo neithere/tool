bf53e836bfa5c340d32238c019adff576bf048ee
Andy Mikhailenko <andy@neithere.net>
2010-07-23 03:45:36 +0600

    Added function get_settings_for_bundle.

--- tool/conf.py
+++ tool/conf.py
@@ -1,14 +1,36 @@
 # -*- coding: utf-8 -*-
 
 """
+Configuration
+=============
+
 The configuration is just a dictionary. It may come from different sources and
 in different formats, so we just provide some shortcuts to simplify things.
+
+Sections
+--------
+
+The configuration can be empty or contain any of these values:
+
+:bundles:
+    a dictionary where keys are module paths and values are whatever these
+    modules expect as settings. It is generally OK to pass `None` as the value
+    when you only need to import the module. The module can then automatically
+    subscribe to desired :doc:`signals`. Please refer to there modules'
+    documentation for details. A number of such modules ships with Tool as
+    :doc:`ext`.
+:debug:
+    if True, the WSGI application is wrapped in DebugMiddleware.
+
+API reference
+-------------
 """
 
+from copy import deepcopy
 import os.path
 from tool.importing import import_attribute
 
-__all__ = ['ConfigurationError', 'load']
+__all__ = ['ConfigurationError', 'get_settings_for_bundle', 'load']
 
 
 FORMATS = {
@@ -26,9 +48,11 @@ def load(path, format=None):
     Expects a filename, returns a dictionary. Raises ConfigurationError if
     the file could not be read or parsed.
 
-    :param path: path to the file.
-    :param format: format in which the configuration dictionary in serialized
-        in the file (one of: "yaml", "json").
+    :param path:
+        path to the file.
+    :param format:
+        format in which the configuration dictionary in serialized in the file
+        (one of: "yaml", "json").
 
     """
     if not os.path.exists(path):
@@ -64,3 +88,44 @@ def load(path, format=None):
         raise ConfigurationError('Deserialized config must be a dict, got "%s"'
                                  % conf)
     return conf
+
+def get_settings_for_bundle(settings, path, default=None):
+    """
+    Extracts and returns settings for given bundle from given configuration
+    dictionary.
+
+    Raises `KeyError` if the settings dictionary does not specify anything for
+    this bundle or the `bundles` section at all. Empty (but existing) bundle
+    settings do not raise the exception.
+
+    :param settings:
+        a settings dictionary, must have the key "bundles" with a
+        sub-dictionary with bundle module path as keys. There are no
+        constraints on values. It is OK to pass `None`.
+    :param path:
+        the *full* path to the module that represents a bundle (e.g.
+        `tool.ext.jinja`).
+
+    Example settings (in YAML)::
+
+        bundles:
+            tool.ext.templating:
+                searchpaths: ['templates/']
+            tool.ext.documents: nil    # --> `None` in Python
+
+    .. note::
+
+        The returned value is a deep copy of the original value. This allows
+        the client code to modify the value (e.g. use `conf.pop('foo')`)
+        without breaking the orginal configuration.
+
+    """
+    try:
+        bundles = settings['bundles']
+    except KeyError:
+        raise KeyError('There is no section "bundles" in the settings.')
+    if path in bundles:
+        return deepcopy(bundles[path])
+    if default is not None:
+        return default
+    raise KeyError('Bundle "{path}" is not in settings'.format(**locals()))
